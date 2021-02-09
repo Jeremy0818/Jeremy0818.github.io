72e6fe25748f1ab3a6d84d724fa0cfe6757a21e0
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-02-08 20:42:42 -0700

    finished customizing my cool site

---
+++ content/my-second-page.md
@@ -0,0 +1,7 @@
+Title: My Second Page
+Date: 2021-02-08 7:00
+Category: Page
+
+Is it working? Congrats if you see me!
+
+small paragraph
--- publishconf.py
+++ publishconf.py
@@ -10,11 +10,11 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://github.com/Jeremy0818/Jeremy0818.github.io'
+SITEURL = 'https://jeremy0818.github.io/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
